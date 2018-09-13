f6d9b7b1cc62b3c53d4664117697a33b98ae4103
Lemeunier Clément <clement.lemeunier@etu.univ-lyon1.fr>
2018-09-13 22:32:36 +0200

    Correction test

--- lab2-tools/poneymon_fx/src/test/java/fr/univ_lyon1/info/m1/poneymon_fx/PoneyTest.java
+++ lab2-tools/poneymon_fx/src/test/java/fr/univ_lyon1/info/m1/poneymon_fx/PoneyTest.java
@@ -1,4 +1,5 @@
 package fr.univ_lyon1.info.m1.poneymon_fx;
+package fr.univ_lyon1.info.m1.poneymon_fx.Model;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
