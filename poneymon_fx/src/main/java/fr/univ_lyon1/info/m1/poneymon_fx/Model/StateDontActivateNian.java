e17fa1cb3141d97fc739d122e068ce1cb47dfabb
Lemeunier Clément <clement.lemeunier@etu.univ-lyon1.fr>
2018-09-27 15:31:06 +0200

    Correction

--- poneymon_fx/src/main/java/fr/univ_lyon1/info/m1/poneymon_fx/Model/StateDontActivateNian.java
+++ poneymon_fx/src/main/java/fr/univ_lyon1/info/m1/poneymon_fx/Model/StateDontActivateNian.java
@@ -1,10 +1,9 @@
 package fr.univ_lyon1.info.m1.poneymon_fx.Model;
 
 public class StateDontActivateNian implements StateLike {
-
     
     /**
-     * Activates the nian mode for IA poneys if some conditions are satisfied.
+     * Activates the nian mode for poneys if some conditions are satisfied.
      * 
      * @param context.
      * @param poney.
